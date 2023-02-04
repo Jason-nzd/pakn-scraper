21721404156c3358ba013a119756132d0ef08074
Jason Burgess <jason.nzd@gmail.com>
2023-02-04 16:39:17 +1300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Playwright;
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync();
+var page = await browser.NewPageAsync();
+
+await page.GotoAsync("https://www.paknsave.co.nz/shop/category/fresh-foods-and-bakery?pg=1");
+
+var cards = page.Locator("div.fs-product-card");
+
+var html = cards.InnerHTMLAsync();
+var query = await page.QuerySelectorAllAsync("div.fs-product-card");
+var l = query.ToList();
+
+// public record Product(string name, string id, float currentPrice, string size);
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(html.ToString());
