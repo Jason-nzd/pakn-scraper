a370829e2030e90536fe6f1fd06b7eb278edc971
Jason Burgess <jason.nzd@gmail.com>
2023-02-20 23:21:41 +1300

    added azure cosmosdb support

---
+++ CosmosDB.cs
@@ -0,0 +1,161 @@
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using static PakScraper.Program;
+
+namespace PakScraper
+{
+    public partial class CosmosDB
+    {
+        public static async Task<Boolean> establishConnection(string databaseName, string partitionKey, string containerName)
+        {
+            try
+            {
+                // Read from appsettings.json or appsettings.local.json
+                IConfiguration config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+
+                cosmosClient = new CosmosClient(
+                    accountEndpoint: config.GetRequiredSection("COSMOS_ENDPOINT").Get<string>(),
+                    authKeyOrResourceToken: config.GetRequiredSection("COSMOS_KEY").Get<string>()!
+                );
+
+                database = cosmosClient.GetDatabase(id: databaseName);
+
+                // Container reference with creation if it does not already exist
+                cosmosContainer = await database.CreateContainerIfNotExistsAsync(
+                    id: containerName,
+                    partitionKeyPath: partitionKey,
+                    throughput: 400
+                );
+
+                log(ConsoleColor.Yellow, $"\n(Connected to CosmosDB) {cosmosClient.Endpoint}");
+                return true;
+            }
+            catch (Microsoft.Azure.Cosmos.CosmosException e)
+            {
+                log(ConsoleColor.Red, e.GetType().ToString());
+                log(ConsoleColor.Red,
+                "Error Connecting to CosmosDB - check appsettings.json, endpoint or key may be expired");
+                return false;
+            }
+            catch (HttpRequestException e)
+            {
+                log(ConsoleColor.Red, e.GetType().ToString());
+                log(ConsoleColor.Red,
+                "Error Connecting to CosmosDB - check firewall and internet status");
+                return false;
+            }
+            catch (Exception e)
+            {
+                log(ConsoleColor.Red, e.GetType().ToString());
+                log(ConsoleColor.Red,
+                "Error Connecting to CosmosDB - make sure appsettings.json is created and contains:");
+                log(ConsoleColor.White,
+                    "{\n" +
+                    "\t\"COSMOS_ENDPOINT\": \"<your cosmosdb endpoint uri>\",\n" +
+                    "\t\"COSMOS_KEY\": \"<your cosmosdb primary key>\"\n" +
+                    "}\n"
+                );
+                return false;
+            }
+        }
+
+        // Takes a scraped Product, and tries to insert or update an existing Product on CosmosDB
+        public async static Task<UpsertResponse> upsertProduct(Product scrapedProduct)
+        {
+            bool productAlreadyOnCosmosDB = false;
+            Product? dbProduct = null;
+
+            try
+            {
+                // Check if product already exists on CosmosDB, throws exception if not found
+                var response = await cosmosContainer!.ReadItemAsync<Product>(
+                    scrapedProduct.id,
+                    new PartitionKey(scrapedProduct.name)
+                );
+
+                // Set local product from CosmosDB resource
+                dbProduct = response.Resource;
+                if (response.StatusCode == System.Net.HttpStatusCode.OK) productAlreadyOnCosmosDB = true;
+            }
+            catch (Microsoft.Azure.Cosmos.CosmosException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    productAlreadyOnCosmosDB = false;
+            }
+
+            if (productAlreadyOnCosmosDB)
+            {
+                // Check if price has changed
+                float dbPrice = dbProduct!.currentPrice;
+                float scrapedPrice = scrapedProduct.currentPrice;
+
+                if (dbPrice != scrapedPrice)
+                {
+                    // Price has changed, so we can create an updated Product with the changes
+                    DatedPrice[] updatedHistory = dbProduct.priceHistory;
+                    updatedHistory.Append(scrapedProduct.priceHistory[0]);
+
+                    Product updatedProduct = new Product(
+                        dbProduct.id,
+                        dbProduct.name,
+                        scrapedProduct.currentPrice,
+                        dbProduct.category,
+                        dbProduct.size,
+                        dbProduct.sourceSite,
+                        updatedHistory,
+                        dbProduct.imgUrl
+                    );
+
+                    // Log price change with different verb and colour depending on price change direction
+                    bool priceTrendingDown = (scrapedPrice < dbPrice);
+                    string priceTrendText = "Price " + (priceTrendingDown ? "Decreased" : "Increased").PadLeft(15);
+
+                    log(priceTrendingDown ? ConsoleColor.Green : ConsoleColor.Red,
+                        $"{priceTrendText} {dbProduct.name.PadRight(40).Substring(0, 40)} from " +
+                        $"${dbProduct.currentPrice} to ${scrapedProduct.currentPrice}"
+                    );
+
+                    try
+                    {
+                        // Upsert the updated product back to CosmosDB
+                        await cosmosContainer!.UpsertItemAsync<Product>(updatedProduct, new PartitionKey(updatedProduct.name));
+                        return UpsertResponse.Updated;
+                    }
+                    catch (Microsoft.Azure.Cosmos.CosmosException e)
+                    {
+                        Console.WriteLine($"CosmosDB Upsert Error on existing Product: {e.StatusCode}");
+                        return UpsertResponse.Failed;
+                    }
+                }
+                else
+                {
+                    return UpsertResponse.AlreadyUpToDate;
+                }
+            }
+            else
+            {
+                try
+                {
+                    // No existing product was found, upload to CosmosDB
+                    await cosmosContainer!.UpsertItemAsync<Product>(scrapedProduct, new PartitionKey(scrapedProduct.name));
+
+                    Console.WriteLine(
+                        $"{"New Product:".PadLeft(15)} {scrapedProduct.id.PadRight(8)} | " +
+                        $"{scrapedProduct.name!.PadRight(40).Substring(0, 40)}" +
+                        $" | ${scrapedProduct.currentPrice}\t\t| {scrapedProduct.category.Last()}"
+                    );
+
+                    return UpsertResponse.NewProduct;
+                }
+                catch (Microsoft.Azure.Cosmos.CosmosException e)
+                {
+                    Console.WriteLine($"{"CosmosDB:".PadLeft(15)} Upsert Error for new Product: {e.StatusCode}");
+                    return UpsertResponse.Failed;
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,197 +1,274 @@
 ﻿using Microsoft.Playwright;
-//using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Cosmos;
 
-public class PakScraper
+namespace PakScraper
 {
-    static bool dryRunMode = true;
-    static int secondsDelayBetweenPageScrapes = 22;
-
-    static string[] urls = new string[] {
-        "https://www.paknsave.co.nz/shop/category/fresh-foods-and-bakery?pg=1",
-        "https://www.paknsave.co.nz/shop/category/chilled-frozen-and-desserts?pg=1"
-    };
-
-    public record DatedPrice(string date, float price);
-    public record Product(string id, string name, float currentPrice, string size, string sourceSite, DatedPrice[] priceHistory, string imgUrl);
-
-    enum UpsertResponse
+    public class Program
     {
-        NewProduct,
-        Updated,
-        AlreadyUpToDate,
-        Failed
-    }
+        static int secondsDelayBetweenPageScrapes = 22;
+        static string[] urls = new string[] {
+            "https://www.paknsave.co.nz/shop/category/fresh-foods-and-bakery?pg=1",
+            "https://www.paknsave.co.nz/shop/category/chilled-frozen-and-desserts?pg=1"
+        };
 
-    public static async Task Main(string[] args)
-    {
-        // Handle arguments - 'dotnet run dry' will run in dry mode
-        if (args.Length > 0)
+        public record Product(
+            string id,
+            string name,
+            float currentPrice,
+            string[] category,
+            string size,
+            string sourceSite,
+            DatedPrice[] priceHistory,
+            string imgUrl
+        );
+
+        public record DatedPrice(
+            string date,
+            float price
+        );
+
+        public static CosmosClient? cosmosClient;
+        public static Database? database;
+        public static Container? cosmosContainer;
+        public static IPage? playwrightPage;
+
+        public static async Task Main(string[] args)
         {
-            if (args[0] == "dry") dryRunMode = true;
+            // Handle arguments - 'dotnet run dry' will run in dry mode, bypassing CosmosDB
+            if (args.Length > 0)
+            {
+                if (args[0] == "dry") dryRunMode = true;
+                log(ConsoleColor.Yellow, $"\n(Dry Run mode on)");
+            }
+
+            // Launch Playwright Browser
+            var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(
+                new BrowserTypeLaunchOptions { Headless = false }
+            );
+            playwrightPage = await browser.NewPageAsync();
+            await routePlaywrightExclusions(logToConsole: false);
+
+            // Connect to CosmosDB - end program if unable to connect
+            if (!dryRunMode)
+            {
+                if (!await CosmosDB.establishConnection(
+                       databaseName: "supermarket-prices",
+                       partitionKey: "/name",
+                       containerName: "supermarket-products"
+                   )) return;
+            }
+
+            // Open up each URL and run the scraping function
+            for (int i = 0; i < urls.Count(); i++)
+            {
+                // Try load page and wait for full content to dynamically load in
+                try
+                {
+                    log(ConsoleColor.Yellow,
+                        $"\nLoading Page [{i + 1}/{urls.Count()}] {urls[i].PadRight(112).Substring(12, 100)}");
+                    await playwrightPage!.GotoAsync(urls[i]);
+                    await playwrightPage.WaitForSelectorAsync("span.fs-price-lockup__cents");
+                }
+                catch (System.Exception e)
+                {
+                    log(ConsoleColor.Red, "Unable to Load Web Page");
+                    Console.Write(e.ToString());
+                    return;
+                }
+
+                // Query all product card entries
+                var productElements = await playwrightPage.QuerySelectorAllAsync("div.fs-product-card");
+                log(ConsoleColor.Yellow, productElements.Count.ToString().PadLeft(12) + " products found");
+
+                // Create counters for logging purposes
+                int newProductsCount = 0, updatedProductsCount = 0, upToDateProductsCount = 0;
+
+                // Loop through every found playwright element
+                foreach (var element in productElements)
+                {
+                    // Create Product object from playwright element
+                    Product scrapedProduct = await scrapeProductElementToRecord(element, urls[i]);
+
+                    if (!dryRunMode)
+                    {
+                        // Try upsert to CosmosDB
+                        UpsertResponse response = await CosmosDB.upsertProduct(scrapedProduct);
+
+                        // Increment stats counters based on response from CosmosDB
+                        switch (response)
+                        {
+                            case UpsertResponse.NewProduct:
+                                newProductsCount++;
+                                break;
+
+                            case UpsertResponse.Updated:
+                                updatedProductsCount++;
+                                break;
+
+                            case UpsertResponse.AlreadyUpToDate:
+                                upToDateProductsCount++;
+                                break;
+
+                            case UpsertResponse.Failed:
+                            default:
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        // In Dry Run mode, print a log row for every product
+                        Console.WriteLine(
+                            scrapedProduct.id.PadLeft(9) + " | " + scrapedProduct.name!.PadRight(40).Substring(0, 40) +
+                            " | " + scrapedProduct.size.PadRight(12) + " | $" + scrapedProduct.currentPrice + "\t| " +
+                            scrapedProduct.category.Last().PadRight(10)
+                        );
+                    }
+                }
+
+                // Log consolidated CosmosDB stats for entire page scrape
+                if (!dryRunMode)
+                {
+                    log(ConsoleColor.Blue, $"{"CosmosDB:".PadLeft(15)} {newProductsCount} new products, " +
+                    $"{updatedProductsCount} updated, {upToDateProductsCount} already up-to-date");
+                }
+
+                // This page has now completed scraping. A delay is added in-between each subsequent URL
+                if (i != urls.Count() - 1)
+                {
+                    log(ConsoleColor.Gray,
+                        $"{"Waiting".PadLeft(13)} {secondsDelayBetweenPageScrapes}s until next page scrape.."
+                    );
+                    Thread.Sleep(secondsDelayBetweenPageScrapes * 1000);
+                }
+            }
+
+            // Clean up playwright browser and end program
+            log(ConsoleColor.Blue, "\nScraping Completed \n");
+            await browser.CloseAsync();
+            return;
         }
 
-        // If dry run mode on, skip CosmosDB
-        if (dryRunMode) log(ConsoleColor.Yellow, $"\n(Dry Run mode on)");
-
-        // Launch Playwright Browser
-        using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync(
-            new BrowserTypeLaunchOptions { Headless = false }
-        );
-        var page = await browser.NewPageAsync();
-
-        // Define excluded types and urls to reject
-        string[] typeExclusions = { "image", "stylesheet", "media", "font", "other" };
-        string[] urlExclusions = { "googleoptimize.com", "gtm.js", "visitoridentification.js", "js-agent.newrelic.com", "challenge" };
-        List<string> exclusions = urlExclusions.ToList<string>();
-
-        // Route with exclusions processed
-        await page.RouteAsync("**/*", async route =>
+        // Takes a playwright element "div.fs-product-card", scrapes each of the desired data fields,
+        //  and then returns a completed Product record
+        async static Task<Product> scrapeProductElementToRecord(IElementHandle element, string sourceUrl)
         {
-            var req = route.Request;
-            bool excludeThisRequest = false;
-            string trimmedUrl = req.Url.Length > 120 ? req.Url.Substring(0, 120) + "..." : req.Url;
+            // Name
+            var aTag = await element.QuerySelectorAsync("a");
+            string? name = await aTag!.GetAttributeAsync("aria-label");
 
-            foreach (string exclusion in exclusions)
-            {
-                if (req.Url.Contains(exclusion)) excludeThisRequest = true;
-            }
+            // Image URL
+            var imgDiv = await aTag.QuerySelectorAsync("div div");
+            string? imgUrl = await imgDiv!.GetAttributeAsync("data-src-s");
+            imgUrl = imgUrl!.Replace("200x200", "400x400");     // get the higher-res version
 
-            if (excludeThisRequest)
-            {
-                //log(ConsoleColor.Red, $"{req.Method} {req.ResourceType} - {trimmedUrl}");
-                await route.AbortAsync();
-            }
-            else
-            {
-                //log(ConsoleColor.White, $"{req.Method} {req.ResourceType} - {trimmedUrl}");
-                await route.ContinueAsync();
-            }
-        });
+            // ID
+            var imageFilename = imgUrl!.Split("/").Last();  // get original ID from image url
+            string id = "P" + imageFilename.Split(".").First();   // prepend P to ID
 
-        // Open up each URL and run the scraping function
-        await openEachURLForScraping(urls, page);
+            // Size
+            var pTag = await aTag.QuerySelectorAsync("p");
+            string size = await pTag!.InnerHTMLAsync();
+            size = size.Replace("l", "L");  // capitalize L for litres
 
-        // Complete after all URLs have been scraped
-        log(ConsoleColor.Blue, "\nScraping Completed \n");
+            // Mark source website
+            string sourceSite = "paknsave.co.nz";
 
-        // Clean up playwright browser and end program
-        await browser.CloseAsync();
-        return;
-    }
+            string[]? categories = deriveCategoriesFromUrl(sourceUrl);
 
-    async static Task openEachURLForScraping(string[] urls, IPage page)
-    {
-        int urlIndex = 1;
+            // Price scraping is put in try-catch to better handle edge cases
+            float currentPrice = 0;
+            DatedPrice[] priceHistory = { };
 
-        foreach (var url in urls)
-        {
-            // Try load page and wait for full page to dynamically load in
             try
             {
-                log(ConsoleColor.Yellow, $"\nLoading Page [{urlIndex++}/{urls.Count()}] {url.PadRight(112).Substring(12, 100)}");
-                await page.GotoAsync(url);
+                var dollarSpan = await element.QuerySelectorAsync(".fs-price-lockup__dollars");
+                string dollarString = await dollarSpan!.InnerHTMLAsync();
 
-                await page.WaitForSelectorAsync("span.fs-price-lockup__cents");
+                var centSpan = await element.QuerySelectorAsync(".fs-price-lockup__cents");
+                string centString = await centSpan!.InnerHTMLAsync();
+                currentPrice = float.Parse(dollarString + "." + centString);
+
+                // DatedPrice with date format 'Tue Jan 14 2023'
+                string todaysDate = DateTime.Now.ToString("ddd MMM dd yyyy");
+                DatedPrice todaysDatedPrice = new DatedPrice(todaysDate, currentPrice);
+
+                // Create Price History array with a single element
+                priceHistory = new DatedPrice[] { todaysDatedPrice };
             }
-            catch (System.Exception e)
+            catch (Exception e)
             {
-                log(ConsoleColor.Red, "Unable to Load Web Page");
-                Console.Write(e.ToString());
-                return;
+                log(ConsoleColor.Red, $"Price scrape error on {name}");
+                Console.Write(e);
             }
-
-            // Query all product card entries
-            var productElements = await page.QuerySelectorAllAsync("div.fs-product-card");
-            Console.WriteLine(productElements.Count.ToString().PadLeft(15) + " products found");
-
-            // Loop through every found playwright element
-            foreach (var element in productElements)
-            {
-                // Create Product object from playwright element
-                Product scrapedProduct = await scrapeProductElementToRecord(element);
-
-                // In Dry Run mode, print a log row for every product
-                Console.WriteLine(
-                    scrapedProduct.id.PadLeft(9) + " | " +
-                    scrapedProduct.name!.PadRight(50).Substring(0, 50) + " | " +
-                    scrapedProduct.size.PadRight(12) + " | " +
-                    "$" + scrapedProduct.currentPrice
-                );
-            }
-
-            // This page has now completed scraping. A delay is added in-between each subsequent URL
-            if (urlIndex <= urls.Count())
-            {
-                Console.WriteLine($"{"Waiting".PadLeft(15)} {secondsDelayBetweenPageScrapes}s until next page scrape..");
-                Thread.Sleep(secondsDelayBetweenPageScrapes * 1000);
-            }
+            // Return completed Product record
+            return (new Product(id, name!, currentPrice, categories!, size, sourceSite, priceHistory, imgUrl));
         }
 
-        // All page URLs have completed scraping.
-        return;
-    }
-
-    // Takes a playwright element "div.fs-product-card", scrapes each of the desired data fields,
-    //  and then returns a completed Product record
-    async static Task<Product> scrapeProductElementToRecord(IElementHandle element)
-    {
-
-        // Name
-        var aTag = await element.QuerySelectorAsync("a");
-        string? name = await aTag!.GetAttributeAsync("aria-label");
-
-        // Image URL
-        var imgDiv = await aTag.QuerySelectorAsync("div div");
-        string? imgUrl = await imgDiv!.GetAttributeAsync("data-src-s");
-        imgUrl = imgUrl!.Replace("200x200", "400x400");     // get the higher-res version
-
-        // ID
-        var imageFilename = imgUrl!.Split("/").Last();  // get original ID from image url
-        string id = "P" + imageFilename.Split(".").First();   // prepend P to ID
-
-        // Size
-        var pTag = await aTag.QuerySelectorAsync("p");
-        string size = await pTag!.InnerHTMLAsync();
-        size = size.Replace("l", "L");  // capitalize L for litres
-
-        // Mark source website
-        string sourceSite = "paknsave.co.nz";
-
-        // Price scraping is put in try-catch to better handle edge cases
-        float currentPrice = 0;
-        DatedPrice[] priceHistory = { };
-
-        try
+        // Shorthand function for logging with colour
+        public static void log(ConsoleColor color, string text)
         {
-            var dollarSpan = await element.QuerySelectorAsync(".fs-price-lockup__dollars");
-            string dollarString = await dollarSpan!.InnerHTMLAsync();
-
-            var centSpan = await element.QuerySelectorAsync(".fs-price-lockup__cents");
-            string centString = await centSpan!.InnerHTMLAsync();
-            currentPrice = float.Parse(dollarString + "." + centString);
-
-            // DatedPrice with date format 'Tue Jan 14 2023'
-            string todaysDate = DateTime.Now.ToString("ddd MMM dd yyyy");
-            DatedPrice todaysDatedPrice = new DatedPrice(todaysDate, currentPrice);
-
-            // Create Price History array with a single element
-            priceHistory = new DatedPrice[] { todaysDatedPrice };
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
         }
-        catch (Exception e)
+
+        static string[]? deriveCategoriesFromUrl(string url)
         {
-            log(ConsoleColor.Red, $"Price scrape error on {name}");
-            Console.Write(e);
-        }
-        // Return completed Product record
-        return (new Product(id, name!, currentPrice, size, sourceSite, priceHistory, imgUrl));
-    }
+            // www.domain.co.nz/shop/category/chilled-frozen-and-desserts?pg=1"
+            // If url doesn't contain /browse/, return no category
+            if (url.IndexOf("/category/") < 0) return null;
 
-    // Shorthand function for logging with colour
-    static void log(ConsoleColor color, string text)
-    {
-        Console.ForegroundColor = color;
-        Console.WriteLine(text);
-        Console.ForegroundColor = ConsoleColor.White;
+            int categoriesStartIndex = url.IndexOf("/category/");
+            int categoriesEndIndex = url.Contains("?") ? url.IndexOf("?") : url.Length;
+            string categoriesString = url.Substring(categoriesStartIndex, categoriesEndIndex - categoriesStartIndex);
+            string[] splitCategories = categoriesString.Split("/").Skip(1).ToArray();
+
+            return splitCategories;
+        }
+
+        static async Task routePlaywrightExclusions(bool logToConsole)
+        {
+            // Define excluded types and urls to reject
+            string[] typeExclusions = { "image", "stylesheet", "media", "font", "other" };
+            string[] urlExclusions = { "googleoptimize.com", "gtm.js", "visitoridentification.js",
+                "js-agent.newrelic.com", "challenge-platform" };
+            List<string> exclusions = urlExclusions.ToList<string>();
+
+            // Route with exclusions processed
+            await playwrightPage!.RouteAsync("**/*", async route =>
+            {
+                var req = route.Request;
+                bool excludeThisRequest = false;
+                string trimmedUrl = req.Url.Length > 120 ? req.Url.Substring(0, 120) + "..." : req.Url;
+
+                foreach (string exclusion in exclusions)
+                {
+                    if (req.Url.Contains(exclusion)) excludeThisRequest = true;
+                }
+                if (typeExclusions.Contains(req.ResourceType)) excludeThisRequest = true;
+
+                if (excludeThisRequest)
+                {
+                    if (logToConsole) log(ConsoleColor.Red, $"{req.Method} {req.ResourceType} - {trimmedUrl}");
+                    await route.AbortAsync();
+                }
+                else
+                {
+                    if (logToConsole) log(ConsoleColor.White, $"{req.Method} {req.ResourceType} - {trimmedUrl}");
+                    await route.ContinueAsync();
+                }
+            });
+        }
+
+        public enum UpsertResponse
+        {
+            NewProduct,
+            Updated,
+            AlreadyUpToDate,
+            Failed
+        }
+
+        static bool dryRunMode = false;
     }
 }
